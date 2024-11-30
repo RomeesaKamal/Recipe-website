24e7324d6aa843d478f052f07b7ec12a1a338e68
RomeesaKamal <kamalromeesa@gmail.com>
2024-11-30 21:05:46 +0500

    correction

--- script.js
+++ script.js
@@ -91,9 +91,8 @@ const openRecipePopup = (meal) => {
   
   // Show the recipe details container
 
-  recipeDetailsContent.parentElement.style.display = 'none';
+  recipeDetailsContent.parentElement.style.display = 'block';
  
-  
 };
 
 
